744de26df7053a9f92f01600c8d0b45e0c873fd4
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-07-04 17:41:06 -0300

    Projeto 2 - Formulários e Animações

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+var nome = prompt("Qual seu nome?") //Solicita ao usuário inserir o nome
+
+alert("Olá " + nome + "! Seja bem vindo(a) ao site Aventureiros!"); //Exibe uma mensagem de boas vindas
+
+function msg1(){
+    alert("Excelente escolha! Você garantiu sua vaga!"); //Exibe uma mensagem na tela quando o usuário clicar no botão enviar inscrição. Ainda não valida se todos campos do forms estão preenchidos, muita hora nessa calma!
+}
+
+function msg2(){
+    alert("Que pena! Esperamos te ver em breve!") //Exibe uma mensagem na tela quando o usuário clicar no botão desistir de passear.
+}
